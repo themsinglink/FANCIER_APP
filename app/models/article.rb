a62476011c59c79224200ec040ec29489aafa27e
Meghan Martin <meghanxmartin@gmail.com>
2020-03-03 18:17:33 +0100

    form

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,12 +5,10 @@ class Article < ApplicationRecord
   has_many :tags, through: :article_tags
   has_one_attached :photo
   monetize :price_cents
-<<<<<<< HEAD
   validates :photo, presence: true
   validates :name, presence: true
   validates :name, presence: true
   validates :price_cents, presence: true
-=======
 
   include PgSearch::Model
   pg_search_scope :search_by_name_and_color_and_material_and_category_id,
@@ -19,7 +17,6 @@ class Article < ApplicationRecord
     against: [ :name, :color, :material, :category_id],
     using: {
       tsearch: { prefix: true }     }
->>>>>>> 698e169d75b6dcfb32970ab3031e2a63b8e1b778
 
 end
 
