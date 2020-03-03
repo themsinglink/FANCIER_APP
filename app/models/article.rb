da44b55236ddccbe1254d9c390a090449b0c45eb
Meghan Martin <meghanxmartin@gmail.com>
2020-03-03 18:09:20 +0100

    validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,10 @@ class Article < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_one_attached :photo
-
   monetize :price_cents
+  validates :photo, presence: true
+  validates :name, presence: true
+  validates :name, presence: true
+  validates :price_cents, presence: true
+
 end
