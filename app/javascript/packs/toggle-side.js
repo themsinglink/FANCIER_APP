c4f162b805e27056a68fe9536fb2937f5841d94c
Kendrikc rambal <k.rambal96@gmail.com>
2020-03-04 15:28:02 +0100

    Created the side menu

--- app/javascript/packs/toggle-side.js
+++
@@ -1,6 +0,0 @@
-
-function toggleSide() {
-       var updateElement = document.getElementById("menu-icon");
-    //toggle adds a class if it's not there or removes it if it is.
-    updateElement.classList.toggle("open");
-}
