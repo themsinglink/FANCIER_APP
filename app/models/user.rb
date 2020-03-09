182045ec216c63e63e18f79688a041ecf19cfa79
Laura Aunion <laura.aunion@hotmail.com>
2020-03-09 11:40:03 +0100

    fixed stripe and reviews showing up onarticle

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,15 +11,26 @@ class User < ApplicationRecord
   has_many :favorites
   has_many :favorites, dependent: :destroy
 
-def favorite(article)
- favorites.find_or_create_by(article: article)
-end
+  def favorite(article)
+   favorites.find_or_create_by(article: article)
+  end
 
-def unfavorite(article)
- favorites.where(article: article).destroy_all
+  def unfavorite(article)
+   favorites.where(article: article).destroy_all
 
- article.reload
-end
+   article.reload
+  end
+
+  def rating
+    ratings = []
+    self.articles.each do |article|
+      if article.orders&.first&.review
+        ratings << article&.orders&.first&.review&.rating
+      end
+    end
+    ratings.size >= 1 ? rating = ratings.sum / ratings.size : rating = 0
+    return rating
+  end
 
 
 end
