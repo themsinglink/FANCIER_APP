f240e02e954baab07fb43162e0ceb6ba4704c0ef
Katy Link <ktelink@gmail.com>
2020-03-11 11:36:28 +0100

    meta tag structure set up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,10 +14,17 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
+
   private
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
 end
+
 
